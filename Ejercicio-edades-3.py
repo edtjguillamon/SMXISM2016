e0d4a8dec6f32f1ada3eec445ba32e3f140ccd35
edtjguillamon <edtjguillamon@gmail.com>
2017-03-06 21:50:32 +0100

    Ejercicio de edades 3

---
+++ Ejercicio-edades-3.py
@@ -0,0 +1,7 @@
+#Ejercicio-edad-3:
+#coding utf-8-
+edad =input("Introduce tu edad: ")
+if (edad >=18 and edad<=23) or edad==17:
+	print "Adelante, controla tu cubata!!" 
+else :
+	print "No cumples la edad, fuera :/" 
