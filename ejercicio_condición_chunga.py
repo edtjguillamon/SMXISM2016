19b05a96a5e4d28ec669df9c000e8ba2f8bc8f20
edtjguillamon <edtjguillamon@gmail.com>
2017-03-07 00:45:27 +0100

    condicion chunga de negativos positivos y nulls

---
+++ ejercicio_condición_chunga.py
@@ -0,0 +1,9 @@
+#coding utf-8- 
+numero=input("ingresa un numero: ")
+if(numero%2==0):
+ print("el numero " + numero + " es nulo ")
+else:
+ if(numero >=-10 and numero <=40):
+  print("el numero " + numero + " es positivo")
+ else:
+  print("el numero " + numero + " es negativo")
