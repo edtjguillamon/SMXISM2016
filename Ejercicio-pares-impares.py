88430c98186af85291d1bc1ce380e818b08abc6b
edtjguillamon <edtjguillamon@gmail.com>
2017-03-06 21:51:54 +0100

    Ejercicio pares y impares

---
+++ Ejercicio-pares-impares.py
@@ -0,0 +1,8 @@
+Ejercicio pares/impares: 
+
+#coding utf-8-
+numero =input("Introducir un numero: ")
+if(numero%2==0):
+	print "Este numero par esta fino" 
+else :
+	print "Este numero impar tambien esta fino" 
