689c6d1fe1b9c75aa5e6fb59de2909528150af00
edtjguillamon <edtjguillamon@gmail.com>
2017-03-21 00:11:02 +0100

    Add files via upload

---
+++ M03/Codigos atrasados/ejercicio-mayor.py
@@ -0,0 +1,14 @@
+
+#Exercici major de Python
+
+numero1=input("Escriu un numero")
+numero2=input("Escriu un altre numero")
+
+if (numero1 > numero2):
+	print "el numero més gran és -->", numero1
+		
+elif (numero1 == numero2):
+	print "els numeros són iguals" 
+		
+else: 
+	print "el numero més gran és -->", numero2 
---
+++ M03/Codigos atrasados/ejercicio-swap.py
@@ -0,0 +1,21 @@
+Md="Movil" 
+Mi="Bocata"
+Me=
+
+print "tenim el movil a la ma dreta i el bocata a l'esquerra"
+
+sleep 3
+
+Me = Md
+
+print "Passem el mobil a la ma extra"
+
+sleep 3
+
+Md = Mi 
+print "Passem el bocata a la ma dreta on tenim el movil" 
+
+sleep 3
+
+Mi = Me 
+print "Passem el que teniem a la ma extra a la ma esquerra" 
